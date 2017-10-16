3bd935cd4283fdf5b620922fb11b5714f09f8e2e
javier alvarado <jdrummer1@outlook.com>
2017-10-15 20:55:17 -0700

    Add print.rb file with print and puts examples

---
+++ print.rb
@@ -0,0 +1,5 @@
+puts "This string will print out to the console with a new line after it"
+
+print "This string will print out to the console without a new line after it"
+
+# Use the following command to run this script and view its output in the console: ruby print.rb
